b3948e61dde9dc2e4f7147fe266aa50777e99a3e
KeveXD <100792669+KeveXD@users.noreply.github.com>
2024-05-19 15:40:34 +0200

    joooo

--- masodik/Code_Soft_Alarm-main/lib/Home.dart
+++ masodik/Code_Soft_Alarm-main/lib/Home.dart
@@ -169,27 +169,38 @@ class _MainPageState extends State<MainPage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              "Vegyük kezünkbe a trianoni emlékharangozást!",
+                              "App. leírás:",
                               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                             ),
                             SizedBox(height: 10),
                             Text(
-                              "1920. június 4-én, 16:32 perckor aláírták a trianoni békediktátumot a versailles-i Nagy-Trianon kastély 52 méter hosszú és 7 méter széles folyosóján, a La galérie des Cotelle-ben.",
+                              "http://osszharang.com",
+                              style: TextStyle(fontSize: 16, color: Colors.blue, decoration: TextDecoration.underline),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "Vegyük kezünkbe a trianoni emlékharangozást!\n"
+                                  "1920. június 4-én 16:32 perckor aláírták a trianoni békediktátumot a versailles-i Nagy-Trianon kastély 52 méter hosszú és 7 méter széles folyosóján, a La galérie des Cotelle-ben.\n"
+                                  "Ezen a napon Magyarország elveszítette területének kétharmadát, a magyar népesség egyharmada pedig a határokon kívülre került. Ennek emlékére évekig országszerte megszólaltak a templomharangok ebben az időben. Ez a hagyomány azonban 1945 után teljesen megszűnt. 2012-ben a három, nagy keresztény egyház visszautasította azt a kormányzati kérést, hogy június 4-én, a Nemzeti Összetartozás Napján délután, a trianoni szerződés aláírásának időpontjában konduljanak meg a templomok harangjai, s szóljanak egy percig a megemlékezés részeként.\n"
+                                  "Ezért gondoltuk úgy, hogy saját kezünkbe kell venni ennek a harangozásnak a feladatát.",
                               style: TextStyle(fontSize: 16),
                             ),
                             SizedBox(height: 10),
                             Text(
-                              "Ezen a napon Magyarország elveszítette területének kétharmadát, a magyar népesség egyharmada pedig a határokon kívülre került. Ennek emlékére évekig országszerte megszólaltak a templomharangok ebben az időben. Ez a hagyomány azonban 1945 után teljesen megszűnt. 2012-ben a három, nagy keresztény egyház visszautasította azt a kormányzati kérést, hogy június 4-én, a Nemzeti Összetartozás Napján délután, a trianoni szerződés aláírásának időpontjában konduljanak meg a templomok harangjai, s szóljanak egy percig a megemlékezés részeként.",
+                              "Ez az applikáció nem tesz mást, mint minden évben figyelmezteti\n"
+                                  "használóját az emlékezés szükségességére azzal, hogy június 4-én, 16:32-kor automatikusan megszólaltatja ezt az emlékharangot annyi másodpercre, amennyi az adott trianoni évforduló. Szintén megszólítja tulajdonosát, figyelmeztetve az esemény fontosságára.\n"
+                                  "Miután a trianoni emléknapját és a Nemzeti Összetartozás Napját együtt kell reprezentálnia, ezért ez az applikáció az ÖSSZHARANG nevet kapta. Egyszerre szól az emlékezés és a jövőbe vetett hitünk, összetartozásunk hangján. Bárhol is érjen ezen harangozás pillanata, állj meg egy percre és tartsd magasba a telefonod. Lesznek, akik majd megkérdezik, mire vélhetik ezt a jelenetet. Akkor lehet-kell elmondani, hogy 1920-ban, ebben a pillanatban veszített el Magyarország területének háromnegyedét. Kifejthetjük a szükséges részletességig nemzeti tragédiánk hátterét.\n"
+                                  "Ugyanakkor a harang azokért is szól, akik kint rekedtek a magyar határokon túl és idegen hatalmak, országok polgáraiként élik azóta is életüket. Nem felejtjük el, hogy mindezek ellenére mi összetartozunk. Magyarország, a magyar nemzet egy és oszthatatlan.\n"
+                                  "Ezt az applikációt ki lehet kapcsolni, ha valaki előre látja, hogy\n"
+                                  "a harangozás idején, zavarná az aktuális programjában. A telefon egy héttel, és később egy nappal a harangozást megelőzően még\n"
+                                  "rákérdez a kikapcsolás szükségességére. Kérlek add tovább ennek az applikációnak hírét, hogy minél több honfitárunkkal együtt emlékezhessünk és emlékeztethessünk!\n\n"
+                                  "Szilágyi Ákos – 56 Lángja Alapítvány\n"
+                                  "osszharang.com",
                               style: TextStyle(fontSize: 16),
                             ),
-                            SizedBox(height: 10),
-                            Text(
-                              "Ezért gondoltuk úgy, hogy saját kezünkbe kell venni ennek a harangozásnak \"feladatát.",
-                              style: TextStyle(fontSize: 16),
-                            ),
-                            // A további szövegeket itt folytathatod...
                           ],
                         ),
+
                       ),
                     ),
                   ],
@@ -211,13 +222,16 @@ class _MainPageState extends State<MainPage> {
     DateTime now = DateTime.now();
     DateTime alarmDate = alarm.dateTime;
     Duration difference = alarmDate.difference(now);
+
     int daysUntilAlarm = difference.inDays;
+    int hoursUntilAlarm = difference.inHours.remainder(24);
+    int minutesUntilAlarm = difference.inMinutes.remainder(60);
 
     String countdownText;
-    if (daysUntilAlarm == 0) {
+    if (daysUntilAlarm == 0 && hoursUntilAlarm == 0 && minutesUntilAlarm == 0) {
       countdownText = "Ma";
     } else {
-      countdownText = "$daysUntilAlarm nap";
+      countdownText = "${daysUntilAlarm} nap ${hoursUntilAlarm} óra ${minutesUntilAlarm} perc";
     }
 
     return GestureDetector(
@@ -269,7 +283,9 @@ class _MainPageState extends State<MainPage> {
                       countdownText,
                       style: TextStyle(
                         fontSize: 24,
-                        color: daysUntilAlarm == 0 ? Colors.red : Colors.grey,
+                        color: daysUntilAlarm == 0 && hoursUntilAlarm == 0 && minutesUntilAlarm == 0
+                            ? Colors.red
+                            : Colors.grey,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
@@ -292,7 +308,6 @@ class _MainPageState extends State<MainPage> {
       ),
     );
   }
-
 
 
 
